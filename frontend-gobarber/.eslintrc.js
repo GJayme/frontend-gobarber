bd7df170c5be44a245f09c46cb9f388b6f324c8a
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-10-06 11:05:36 -0300

    feat: configuring eslint/prettier and .env

--- frontend-gobarber/.eslintrc.js
+++ frontend-gobarber/.eslintrc.js
@@ -1,22 +1,32 @@
 module.exports = {
   env: {
-    es6: true,
-    jest: true,
     browser: true,
+    es2021: true,
   },
-  extends: ['plugin:prettier/recommended'],
+  extends: ["airbnb", "prettier", "prettier/react"],
+  parser: "babel-eslint",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
-      modules: true,
-      sourceType: 'module',
     },
+    ecmaVersion: 12,
+    sourceType: "module",
   },
-  plugins: ['react', 'import', 'prettier'],
+  plugins: ["react", "prettier"],
   rules: {
-    'prettier/prettier': 'error',
-    'react/jsx-filename-extension': ['error', { extension: ['.js', '.jsx'] }],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": [
+      "warn",
+      {
+        extensions: [".jsx", ".js"],
+      },
+    ],
+    "import/prefer-default-export": "off",
+    "react/state-in-constructor": "off",
+    "react/static-property-placement": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/prop-types": "off",
+    "no-param-reassign": "off",
+    "no-console": "off",
   },
 };
